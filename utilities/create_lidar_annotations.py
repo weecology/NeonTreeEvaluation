ead72159138a55a6f4eb9f73d647f966e46d58e0
bw4sz <benweinstein2010@gmail.com>
2019-06-27 10:50:00 -0700

    drape annotations MLBS

--- utilities/create_lidar_annotations.py
+++ utilities/create_lidar_annotations.py
@@ -2,15 +2,15 @@ import glob
 import os
 import Lidar
 
-path_to_rgb = "../TEAK/plots/"
-path_to_laz = "../TEAK/plots/"
-path_to_annotations = "../TEAK/annotations/"
+path_to_rgb = "../NIWO/plots/"
+path_to_laz = "../NIWO/plots/"
+path_to_annotations = "../NIWO/annotations/"
 
 #For each .laz file in directory.
 laz_files = glob.glob(path_to_laz+"*.laz")
 
 for laz in laz_files:
-    
+    print(laz)
     #Load laz
     point_cloud = Lidar.load_lidar(laz)
     
@@ -19,8 +19,12 @@ for laz in laz_files:
     basename = os.path.splitext(basename)[0]
     xml_path = os.path.join(path_to_annotations,basename + ".xml")
     
-    #Load annotations and get utm bounds from tif image
-    annotations= Lidar.load_xml(xml_path, path_to_rgb, res=0.1)
+    if (os.path.exists(xml_path)):
+        #Load annotations and get utm bounds from tif image
+        annotations= Lidar.load_xml(xml_path, path_to_rgb, res=0.1)
+    else:
+        print("{} does not exist, skipping image".format(xml_path))
+        continue
     
     #Create boxes
     boxes = Lidar.create_boxes(annotations)
