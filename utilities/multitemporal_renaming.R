5215ae413fd08944746b522cf8586fb1b1823a2a
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-26 12:41:45 -0800

    add missing 2018 plots

--- utilities/multitemporal_renaming.R
+++ utilities/multitemporal_renaming.R
@@ -1,17 +1,17 @@
 library(stringr)
-data_dir<-"/Users/benweinstein/Downloads/temporal/2019/"
+data_dir<-"/Users/benweinstein/Downloads/temporal/2018/"
 fils<-list.files(paste(data_dir,"Camera/",sep=""),full.names = T)
 
-year = "2019"
+year = "2018"
 
 #get sites
 for(x in fils){
 
-  #new_name = str_replace(x, ".tif", paste("_",year,".tif",sep=""))
-  #file.rename(x, new_name)
+  new_name = str_replace(x, ".tif", paste("_",year,".tif",sep=""))
+  file.rename(x, new_name)
 
   #get basename
-  basename <- str_match(x,"(\\w+)_\\d+.tif")[,2]
+  basename <- str_match(x,"(\\w+).tif")[,2]
 
   #rename laz
   old_filename <- paste(data_dir,"LiDAR/",basename,".laz",sep="")
